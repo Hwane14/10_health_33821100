b15ff9d5f0ed0cb166fe937914957ae73456823f
Hamidou Wane <wanen717@gmail.com>
2025-12-11 20:22:12 +0000

    Add bargain gym gear list feature

--- routes/gear.js
+++ routes/gear.js
@@ -13,4 +13,21 @@ module.exports = function(app, appData) {
             });
         });
     });
+
+    // GET list bargain gym gear
+    app.get('/gear/bargain-list', function(req, res, next) {
+        let sqlquery = "SELECT * FROM gym_gear WHERE price < 40";
+
+        // Execute SQL query
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                return next(err);
+            }
+
+            res.render('bargain_gear.ejs', {
+                bargain_gear: result,
+                appData: appData
+            });
+        });
+    });
 }
